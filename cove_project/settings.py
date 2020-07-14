e876d727ccd2da3b07c81205906c828a8cd54863
Sim <sim@spendnetwork.com>
2020-07-14 09:40:37 +0200

    Deploy to heroku (#1)
    * Add web server and Procfile to run site
    
    * Add whitenoise for serving files with heroku
    
    * Add waitress and whitenoise for heroku to pick up
    
    * Allow the setting of ALLOWED_HOSTS as an env var
    
    * Remove the dealer middleware
    
    Heroku apps don't appear to deploy git repos in production.
    
    * Swap out SQLite for Postgres
    
    apps and filesystems are ephemeral
    
    * Add the missing postgres drivers
    
    * SQUASHME you need TWO equals in requirement files
    
    Co-authored-by: Chris Adams <chris@productscience.co.uk>

--- cove_project/settings.py
+++ cove_project/settings.py
@@ -29,10 +29,12 @@ HOTJAR = {
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-DEALER_TYPE = settings.DEALER_TYPE
+# Heroku doesn't have git support when deploying
+DEALER_TYPE = 'null'
+
 SECRET_KEY = settings.SECRET_KEY
 DEBUG = settings.DEBUG
-ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -59,8 +61,9 @@ MIDDLEWARE_CLASSES = (
     "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
-    "dealer.contrib.django.Middleware",
+    # "dealer.contrib.django.Middleware",
     "cove.middleware.CoveConfigCurrentApp",
 )
 
@@ -77,7 +80,7 @@ WSGI_APPLICATION = "cove_project.wsgi.application"
 # ... otherwise the files appear under the BASE_DIR that is the Cove library install.
 # That could get messy. We want them to appear in our directory.
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": env("DB_NAME")}
+    'default': env.db()
 }
 
 # Password validation
