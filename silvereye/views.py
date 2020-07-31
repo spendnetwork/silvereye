f83589f186c7c380aa115d1a5e9c6f95c8ab3864
Chris Mytton <mail@chrismytton.uk>
2020-07-31 16:48:47 +0100

    Wire up some parts of the publisher page
    This means the correct name is displayed based on the slug.

--- silvereye/views.py
+++ silvereye/views.py
@@ -15,8 +15,11 @@ def publisher_listing(request):
     return render(request, "silvereye/publisher_listing.html", context)
 
 
-def publisher(request):
-    return render(request, "silvereye/publisher.html")
+def publisher(request, publisher_id):
+    context = {
+        'publisher': PublisherMetrics.objects.get(publisher_id=publisher_id),
+    }
+    return render(request, "silvereye/publisher.html", context)
 
 
 # TODO: Remove this once we've moved the styles over to cove-ocds's main upload form.
