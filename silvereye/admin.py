63d9ba33609cc20804a67fd1276b4e448b0ada74
SimKennedy <simkennedy1@gmail.com>
2020-08-17 17:30:38 +0200

    Make PublisherMetricsAdmin read-only

--- silvereye/admin.py
+++ silvereye/admin.py
@@ -25,6 +25,6 @@ admin.site.register(Publisher, PublisherAdmin)
 
 class PublisherMetricsAdmin(admin.ModelAdmin):
     list_display = [field.name for field in PublisherMetrics._meta.get_fields()]
-
+    readonly_fields = [field.name for field in PublisherMetrics._meta.get_fields()]
 
 admin.site.register(PublisherMetrics, PublisherMetricsAdmin)
