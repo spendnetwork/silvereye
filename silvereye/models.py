75316b4b7d9f16f7aa291fc85655b3f3f0a68904
SimKennedy <simkennedy1@gmail.com>
2020-08-19 13:05:42 +0200

    Add FileSubmission model with OneToOne relationship to SuppliedData and ForeignKey to Publisher

--- silvereye/models.py
+++ silvereye/models.py
@@ -1,9 +1,12 @@
+from cove.input.models import SuppliedData
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Publisher(models.Model):
-    publisher_id = models.CharField(max_length=1024, primary_key=True)
-    publisher_name = models.CharField(max_length=1024)
+    publisher_id = models.CharField(max_length=1024, null=True)
+    publisher_name = models.CharField(max_length=1024, null=True)
     contact_name = models.CharField(max_length=1024, null=True, blank=True, default="")
     contact_email = models.CharField(max_length=1024, null=True, blank=True, default="")
     contact_telephone = models.CharField(max_length=1024, null=True, blank=True, default="")
@@ -26,3 +29,17 @@ class PublisherMetrics(models.Model):
     class Meta:
         app_label = 'silvereye'
         db_table = 'silvereye_publisher_metrics'
+
+
+class FileSubmission(models.Model):
+    supplied_data = models.OneToOneField(SuppliedData, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=False)
+
+    @receiver(post_save, sender=SuppliedData)
+    def create_file_submission(sender, instance, created, **kwargs):
+        if created:
+            FileSubmission.objects.create(supplied_data=instance)
+
+    @receiver(post_save, sender=SuppliedData)
+    def save_file_submission(sender, instance, **kwargs):
+        instance.filesubmission.save()
---
+++ silvereye/tests/test_models.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.test import TestCase
+
+from cove.input.models import SuppliedData
+
+from silvereye.models import FileSubmission, Publisher
+
+class FileSubmissionTestCase(TestCase):
+    def setUp(self):
+      supplied_data = SuppliedData.objects.create(id=1)
+
+    def test_creation(self):
+      supplied_data = SuppliedData.objects.get(id=1)
+      self.assertIsNotNone(supplied_data.filesubmission)
+
+    def test_saving(self):
+      supplied_data = SuppliedData.objects.get(id=1)
+      publisher = Publisher.objects.create(publisher_name='A Publisher')
+      supplied_data.filesubmission.publisher = publisher
+      supplied_data.save()
+
+      supplied_data = SuppliedData.objects.get(id=1)
+      self.assertEqual(supplied_data.filesubmission.publisher, publisher)
