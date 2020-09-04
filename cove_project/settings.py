2f31780179d1fb10b8952cd68c014c985dae54c4
SimKennedy <simkennedy1@gmail.com>
2020-09-04 09:40:16 +0200

    Update to django-bootstrap4

--- cove_project/settings.py
+++ cove_project/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap3",
+    "bootstrap4",
     "cove",
     "cove.input",
     "cove_ocds",
