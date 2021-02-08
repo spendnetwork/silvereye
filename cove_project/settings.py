0ef0b40bf6fb745aed3b1a650efd531c5f35e1f1
Jason Kannemeyer <vonhitl@gmail.com>
2021-02-08 16:02:53 +0200

    updated files to reflect magiclink installation

--- cove_project/settings.py
+++ cove_project/settings.py
@@ -120,8 +120,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '465'
-EMAIL_HOST_USER = 'telkomlms@gmail.com'
-EMAIL_HOST_PASSWORD =  'T3lk0mlms'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =  ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
